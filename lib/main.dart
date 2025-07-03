daf6279a7331946121e0dd04a930d88a7de983e2
Hiroaki Iwamori <morizo0505@gmail.com>
2025-07-03 14:54:47 +0900

    Supabaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wrestle_lab/features/home/screens/home_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_API_KEY']!,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
